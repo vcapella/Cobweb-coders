44731d1d4775b2f10bfa5dfc26013f92fc840656
ArshleenKNagpal <90290536+ArshleenKNagpal@users.noreply.github.com>
2021-10-28 15:55:31 -0400

    server update

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
 const path = require('path');
 const express = require('express');
-const route = require('./controller');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(route);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUnitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+const hbs=exphbs.create({})
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+// maybe this is false ^^^
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(route);
+// is this necessary? ^^^
+
+// app.use(require('./controllers/'));
 
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => {
