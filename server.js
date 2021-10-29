d10f2597d3079a00a4a166d837424361bc8e69aa
Summer <svilleneuve1994@gmail.com>
2021-10-29 13:25:08 -0400

    handlebar feature

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const path = require('path');
 const express = require('express');
+const handlebar = require('express-handlebars');
 const route = require('./controller');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = handlebar.create({ 
+});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
