43ca1505716c872d135cc7b6c481abf9a78c8f0a
Victor Capella <capella89@outlook.com>
2021-10-29 08:54:58 -0400

    Seeds fixed

--- server.js
+++ server.js
@@ -1,43 +1,43 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const sequelize = require('./config/connection');
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUnitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUnitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
 
-const hbs=exphbs.create({})
+const hbs = exphbs.create({});
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 // maybe this is false ^^^
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // app.use(route);
 // is this necessary? ^^^
 
 // app.use(require('./controllers/'));
 
-sequelize.sync({force: false}).then(() => {
-    app.listen(PORT, () => {
-        console.log(`listening on ${PORT}`);
-    });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+  });
 });
